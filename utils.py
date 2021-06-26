a3b91c336b3f222e9097e823c8b2aaad33811bb6
Huang-Jim <32083377+Huang-Jim@users.noreply.github.com>
2021-06-26 16:35:14 +0800

    refresh

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Set
 
 import pandas as pd
@@ -29,12 +30,15 @@ def duration_args(date: pd.Series,
         return set(_date)
     # 特定區間 - 只有某個月
     if '-' not in duration_arg:
-        _date = [duration_arg]
+        _date = [datetime.strptime(duration_arg, '%Y/%m').strftime("%Y/%#m")]
         return set(_date)
     # 特定區間 - 有給定初始
     start, end = duration_arg.split('-')
     if not end:  # 如果end為空字串，代表取到date的最新"年/月"
         end = _process_date(date.iloc[-1])
+    # 將使用者輸入統一格式成月份沒有zero-padding
+    start = datetime.strptime(start, '%Y/%m').strftime("%Y/%#m")
+    end = datetime.strptime(end, '%Y/%m').strftime("%Y/%#m")
     _date = pd.date_range(start, end, freq='1M').strftime("%Y/%#m").tolist()
     _date.append(end)
     return set(_date)
